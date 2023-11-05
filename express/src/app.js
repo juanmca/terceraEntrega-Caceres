36feeb838d3a1af992b1d6c1210a1e4991b4e65f
Juan Caceres <manue1984.jc@gmail.com>
2023-11-05 14:38:58 +0100

    correcion statusCode

--- express/src/app.js
+++ express/src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const ProductManager = require("./ProductManager");
 
-
 const path = require("path");
 
 console.clear();
@@ -13,8 +12,6 @@ const app = express();
 
 let archivo = path.join(__dirname, "../../archivos", "archivo.json");
 console.log(__dirname);
-
-/* app.use(express.urlencoded({ extended: true })); */
 
 const productosArchivo = new ProductManager(archivo);
 let productos = [];
@@ -42,20 +39,36 @@ const iniciarServer = async () => {
       res.status(200).json({ filtros: req.query, resultado });
     });
 
+    app.get("/products", (req, res) => {
+      let resultado = productos;
+
+      if (req.query.limit) {
+        const limit = parseInt(req.query.limit);
+        if (isNaN(limit) || limit < 0) {
+          return res.status(400).json({ filtros: req.query, resultado });
+        }
+
+        resultado = resultado.slice(0, limit);
+      }
+
+      res.setHeader("Content-type", "application/json");
+      res.status(200).json({ filtros: req.query, resultado });
+    });
+
     app.get("/products/:id", (req, res) => {
       let id = req.params.id;
-
       id = parseInt(id);
 
       if (isNaN(id)) {
-        return res.send("Error, ingrese un id numerico");
+        return res.status(400).json({ error: "Error, ingrese un id numérico" });
       }
-      resultado2 = productos.find((producto) => {
+
+      let resultado2 = productos.find((producto) => {
         return producto.id === id;
       });
 
       if (!resultado2) {
-        return res.send("Error, producto no encontrado");
+        return res.status(404).json({ error: "Error, producto no encontrado" });
       }
 
       res.setHeader("Content-type", "application/json");
@@ -63,9 +76,8 @@ const iniciarServer = async () => {
     });
 
     app.listen(PORT, () => {
-      console.log(`Servidor en linea en puerto ${PORT}`)
-     
-    })
+      console.log(`Servidor en linea en puerto ${PORT}`);
+    });
   } catch (error) {
     console.log("Se ha producido el siguiente error", error.message);
   }
